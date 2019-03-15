e293bec07ae14a327909ba6517bef416110122f3
wisani khosa <wizro@gmx.com>
2019-03-15 23:35:27 +0200

    coded Coordinates

---
+++ app/flyables/Coordinates.java
@@ -0,0 +1,25 @@
+package app.flyables;
+
+public class Coordinates {
+	private int longitude;
+	private int latitude;
+	private int height;
+
+	Coordintes(int longitude, int latitude, int height) {
+		this.longitude = longitude;
+		this.latitude = latitude;
+		this.height = height;
+	}
+
+	public int getLongitude() {
+		return longitude;
+	}
+
+	public int getLatitude() {
+		return latitude;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+}
