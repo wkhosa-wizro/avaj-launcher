d8f9b79adda7894d0ba84f06eba3d15d79f51d09
wisani khosa <wizro@gmx.com>
2019-03-16 09:15:30 +0200

    WeatherTower added implementwtions

--- app/tower/Tower.java
+++ app/tower/Tower.java
@@ -5,7 +5,17 @@ import app.flyable.Flyable;
 
 public abstract class Tower {
 	private ArrayList<Flyable> observers = new ArrayList<Flyable>();
-	public void register(Flyable flyable);
-	public void unregister(Flyable flyable);
-	protected void conditionsChanged();
+	public void register(Flyable flyable) {
+		observers.add(flyable);
+	}
+
+	public void unregister(Flyable flyable) {
+		observers.remove(flyable);
+	}
+
+	protected void conditionsChanged() {
+		for (Flyable flyble : observers) {
+			flyble.updateConditions();
+		}
+	}
 }
