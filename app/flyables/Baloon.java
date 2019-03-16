386bbd1f8e33bbe0e305125b6f577a46c5a2eafe
wisani khosa <wizro@gmx.com>
2019-03-16 09:44:58 +0200

    implemented updateCondition

--- app/flyables/Baloon.java
+++ app/flyables/Baloon.java
@@ -10,7 +10,30 @@ public class Baloon extends Aircraft implements Flyable {
 	}
 
 	public void updateCondition() {
-		// Tobe done
+		String weather = WeatherProvider.getCurrentWeather(
+				coordinates);
+	
+		if (weather.equals("SUN")) {
+			coordinates.setLatitude(coordinates.getLongitude() + 2,
+				coordinates.getLatitude(), 
+						coordinates.getHeight() + 4);
+		
+		} else if (weather.equals("RAIN")) {	
+			coordinates.setLatitude(coordinates.getLongitude(),
+				coordinates.getLatitude(), 
+						coordinates.getHeight() - 5);
+		
+		} else if (weather.equals("FOG")) {	
+			coordinates.setLatitude(coordinates.getLongitude(),
+				coordinates.getLatitude(), 
+						coordinates.getHeight() - 3);
+		
+		} else {	
+			coordinates.setLatitude(coordinates.getLongitude(),
+				coordinates.getLatitude(), 
+					coordinates.getHeight() - 15)
+		}
+	
 	}
 
 	public void registerTower(WeatherTower weatherTower) {
