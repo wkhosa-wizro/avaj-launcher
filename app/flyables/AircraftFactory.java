52cce66718dd9b2c91a7e22640b0d73492b370ae
wisani khosa <wizro@gmx.com>
2019-03-16 10:29:35 +0200

    implemented airncraftFactory

--- app/flyables/AircraftFactory.java
+++ app/flyables/AircraftFactory.java
@@ -1,6 +1,17 @@
 package app.flyables;
 
 public abstract class AircraftFactory {
-	public Flyable newAircraft(String type, String naame, 
-					int longitude, int latitude, int height);
+	public static Flyable newAircraft(String type, String naame, 
+					int longitude, int latitude, int height) {
+		if (type.equals("Baloon")) {
+			return Baloon(name, new Coordinates(longitude,
+				latitude, height));
+		} else (type.equals("Helicopter")) {
+			return Helicopter(name, new Coordinates(longitude, 
+				latitude, height);
+		} else {
+			return JetPlane(name, new Coordinates(longitude, 
+				latitude, heiight));
+		}
+	}
 }
