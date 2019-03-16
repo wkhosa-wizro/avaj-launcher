439276af5282ac5fb32ef907f87a3787defaee45
wisani khosa <wizro@gmx.com>
2019-03-16 09:45:28 +0200

    implemented updateCondition

--- app/flyables/Helicopter.java
+++ app/flyables/Helicopter.java
@@ -10,7 +10,29 @@ public class Helicopter extends Aircraft implements Flyable {
 	}
 
 	public void updateCondition() {
-		// Tobe done
+		String weather = WeatherProvider.getCurrentWeather(
+				coordinates);
+	
+		if (weather.equals("SUN")) {
+			coordinates.setLatitude(coordinates.getLongitude() + 10,
+				coordinates.getLatitude(), 
+						coordinates.getHeight() + 2);
+		
+		} else if (weather.equals("RAIN")) {	
+			coordinates.setLatitude(coordinates.getLongitude() + 5,
+				coordinates.getLatitude(), 
+						coordinates.getHeight());
+		
+		} else if (weather.equals("FOG")) {	
+			coordinates.setLatitude(coordinates.getLongitude() + 1,
+				coordinates.getLatitude(), 
+						coordinates.getHeight());
+		
+		} else {	
+			coordinates.setLatitude(coordinates.getLongitude(),
+				coordinates.getLatitude(), 
+					coordinates.getHeight() - 12)
+		}
 	}
 
 	public void registerTower(WeatherTower weatherTower) {
